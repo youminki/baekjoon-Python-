51d01de1637e4ad412310f147b669b1db7ff2455
youminki <108206432+youminki@users.noreply.github.com>
2023-08-01 16:04:05 +0900

    Update 말이 되고픈 원숭이(1600).py

--- BFS, DFS/말이 되고픈 원숭이(1600).py
+++ BFS, DFS/말이 되고픈 원숭이(1600).py
@@ -7,23 +7,23 @@
 # 원숭이가 이동할 수 있는 위치는 실제 체스에서 말이 이동하는 거리와 같다.
 from collections import deque
 
-K = int(input())
-m,n = map(int,input().split())  # m이 가로 n이 세로
+K = int(input()) # 원숭이의 움직임 횟수를 k로 입력받는다.
+m,n = map(int,input().split())  # m이 가로 n이 세로길이인 맵의 크기를 입력받는다.
 
-#상하좌우
+# 상하좌우 배열 정의
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-#원숭이의 이동경로
+# 원숭이의 이동경로
 mx = [-2, -1, 1, 2, 2, 1, -1, -2]
 my = [1, 2, 2, 1, -1, -2, -2, -1]
-
+# 원숭이의 위치와 움직임 횟수를 저장하는 3차원 배열을 초기화
 visited = [[[0] * m for _ in range(n)] for _ in range(K+1)]
-visited[0][0][0] = 1
+visited[0][0][0] = 1 # 원숭이가 시작 지점을 방문했다고 표시
 
-maps = []
+maps = [] # 맵 정보를 저장할 리스트를 초기화
 for i in range(n): #맵 정보 입력
-    maps.append(list(map(int, input().split())))
-q = deque()
+    maps.append(list(map(int, input().split())))# 맵 정보를 입력받아 리스트에 저장
+q = deque() # q 초기화
 q.append([0,0,0,0]) # x = x좌표
                     # y = y좌표
                     # count = 이동비용(이것을 구하는 이유는 원숭이가 움직인 횟수의 최소값을 구하기 위함)
@@ -47,14 +47,14 @@ def bfs():  # x -> row, y -> col
                     visited[action][nx][ny] = 1
                     q.append((nx, ny, count + 1, action))
 
-        if action < K: # 원숭이의 움직임 이동 (시계방향)
+        if action < K: # 아직 원숭이의 움직임을 사용하지 않은 경우
             for i in range(8):
                 nx = x + mx[i]
                 ny = y + my[i]
-                if 0 <= nx < n and 0 <= ny < m and visited[action + 1][nx][ny] == 0 and maps[nx][ny] == 0:
+                if 0 <= nx < n and 0 <= ny < m and visited[action + 1][nx][ny] == 0 and maps[nx][ny] == 0: # 아직 방문하지 않았고, 이동 가능한 경우에 대해 방문표시를한다.
                     visited[action + 1][nx][ny] = 1
                     q.append((nx, ny, count + 1, action + 1))
     return -1 #큐를 다 돌렸는데 목적지 도착해서 return 되지 않은 경우 -1반환 (더이상 이동할 공간이 없는 것)
 
-if bfs() == -1: # 원숭이의 이동
+if bfs() == -1: # 최촞결과가 -1이면 원숭이가 목적지에 도착하지 못했으므로 -1을 출력한다.
     print(-1)
