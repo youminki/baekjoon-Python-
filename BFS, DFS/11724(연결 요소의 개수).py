b20577a0089d040c9692f9a8fcfd15a5b66dcd9f
youminki <108206432+youminki@users.noreply.github.com>
2023-08-22 16:27:28 +0900

    Create 11724(연결 요소의 개수).py

---
+++ BFS, DFS/11724(연결 요소의 개수).py
@@ -0,0 +1,61 @@
+# 11724(연결 요소의 개수).py
+""" https://www.acmicpc.net/provlem/11724 """
+import sys
+from collections import deque
+
+N, M = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(N + 1)]
+visited = [0] * (N + 1)
+count = 0
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+def BFS(v):
+    queue = deque()
+    queue.append(v)
+    visited[v] = True
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+for i in range(1, N + 1):
+    if visited[i] == False:
+        BFS(i)
+        count += 1
+print(count)
+
+
+#--------------------------------
+# DFS로 푸는 방법
+
+import sys
+
+sys.setrecursionlimit(10000)
+
+N, M = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(N + 1)]
+visited = [0] * (N + 1)
+count = 0
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+def DFS(v):
+    visited[v] = True
+    for i in graph[v]:
+        if visited[i] == False:
+            DFS(i)
+
+for i in range(1, N + 1):
+    if visited[i] == False:
+        DFS(i)
+        count += 1
+print(count)
+
+#--------------------------------
