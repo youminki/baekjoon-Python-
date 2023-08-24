b529019d22a7e98093198d8878e85853cbef4412
youminki <108206432+youminki@users.noreply.github.com>
2023-08-24 20:36:56 +0900

    Update practice.py

--- practice/practice.py
+++ practice/practice.py
@@ -1,23 +1,20 @@
-import sys
+from collections import deque
 
-input = sys.stdin.readline
-N = int(input())
-M = int(input())
-graph = [[] for _ in range(101)]
-visited = [0] * (N + 1)
-
-for  _ in range(M):
-    u, v = map(int, sys.stdin.readline().split())
-    graph[u].append(v)
-    graph[v].append(u)
-def DFS(M):
-    visited[M] = True
-    for i in graph[M]:
-        if visited[i] == 0:
-            DFS(i)
-
-DFS(1)
-print(sum(visited) - 1)
-
-
-
+N, M = map(int, input().split())
+graph = []
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+q = [[0, 0]]
+for i in range(N):
+    graph.append(list(map(int, input())))
+    
+while q:
+    x, y = q.pop(0)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < M:
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx, ny))
+print(graph[N - 1][M - 1])
